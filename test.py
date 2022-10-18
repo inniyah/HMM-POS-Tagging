68593053a94365e18a3a37ef32106adac1f0a589
Miriam Ruiz <miriam@debian.org>
2022-10-18 12:42:05 +0200

    Added textual descriptions of the tags

--- test.py
+++ test.py
@@ -15,6 +15,50 @@ from utils import processing
 CORPUS_PATH = "data/WSJ_02-21.pos"
 alpha = 0.001
 
+TAGS= {
+    'CC': ( 'conjunction, coordinating', 'and, or, but' ),
+    'CD': ( 'cardinal number', 'five, three, 13%' ),
+    'DT': ( 'determiner', 'the, a, these' ),
+    'EX': ( 'existential there', 'there were six boys' ),
+    'FW': ( 'foreign word', 'mais' ),
+    'IN': ( 'conjunction, subordinating or preposition', 'of, on, before, unless' ),
+    'JJ': ( 'adjective', 'nice, easy' ),
+    'JJR': ( 'adjective, comparative', 'nicer, easier' ),
+    'JJS': ( 'adjective, superlative', 'nicest, easiest' ),
+    'LS': ( 'list item marker', ' ' ),
+    'MD': ( 'verb, modal auxillary', 'may, should' ),
+    'NN': ( 'noun, singular or mass', 'tiger, chair, laughter' ),
+    'NNS': ( 'noun, plural', 'tigers, chairs, insects' ),
+    'NNP': ( 'noun, proper singular', 'Germany, God, Alice' ),
+    'NNPS': ( 'noun, proper plural', 'we met two Christmases ago' ),
+    'PDT': ( 'predeterminer', 'both his children' ),
+    'POS': ( 'possessive ending', '\'s' ),
+    'PRP': ( 'pronoun, personal', 'me, you, it' ),
+    'PRP$': ( 'pronoun, possessive', 'my, your, our' ),
+    'RB': ( 'adverb', 'extremely, loudly, hard ' ),
+    'RBR': ( 'adverb, comparative', 'better' ),
+    'RBS': ( 'adverb, superlative', 'best' ),
+    'RP': ( 'adverb, particle', 'about, off, up' ),
+    'SYM': ( 'symbol', '%' ),
+    'TO': ( 'infinitival to', 'what to do?' ),
+    'UH': ( 'interjection', 'oh, oops, gosh' ),
+    'VB': ( 'verb, base form', 'think' ),
+    'VBZ': ( 'verb, 3rd person singular present', 'she thinks' ),
+    'VBP': ( 'verb, non-3rd person singular present', 'I think' ),
+    'VBD': ( 'verb, past tense', 'they thought' ),
+    'VBN': ( 'verb, past participle', 'a sunken ship' ),
+    'VBG': ( 'verb, gerund or present participle', 'thinking is fun' ),
+    'WDT': ( 'wh-determiner', 'which, whatever, whichever' ),
+    'WP': ( 'wh-pronoun, personal', 'what, who, whom' ),
+    'WP$': ( 'wh-pronoun, possessive', 'whose, whosever' ),
+    'WRB': ( 'wh-adverb', 'where, when' ),
+    '.': ( 'punctuation mark, sentence closer', '.;?*' ),
+    ',': ( 'punctuation mark, comma', ',' ),
+    ':': ( 'punctuation mark, colon', ':' ),
+    '(': ( 'contextual separator, left paren', '(' ),
+    ')': ( 'contextual separator, right paren', ')' ),
+}
+
 def parse_argument():
     parser = argparse.ArgumentParser(description='Predict Part of Speech Tags')
     parser.add_argument('--sent', help='Enter your sentence.')
@@ -48,7 +92,8 @@ def predict(sample):
     res = []
     for tok, tag in zip(prep_tokens[:-1], pred[:-1]):
         res.append((tok, tag))
-    print(res)
+    for tok, tag in res:
+        print(f"{tok}\t{tag}\t{TAGS[tag][0]} ({TAGS[tag][1]})")
 
 if __name__ == "__main__":
     args = parse_argument()
